226911fc09cece164c3cb50b4ed70400218da8cc
emil4144 <manvelyan.emil@mail.ru>
2019-06-14 18:15:36 +0400

    validation

--- src/app/admin/message/message.component.ts
+++ src/app/admin/message/message.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AdminService} from '../admin.service'
+import { validate } from 'codelyzer/walkerFactory/walkerFn';
 
 @Component({
   selector: 'app-message',
@@ -16,19 +17,34 @@ export class MessageComponent implements OnInit {
 
   ngOnInit() {
   }
+  validateMail(m){
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(m).toLowerCase());    
+  }
 
   sendMessage(){
-    let data={
-      email:this.mail,
-      mess:this.text
+    if(!this.mail || !this.validateMail(this.mail)){
+      this.msgs=[]
+      this.msgs.push({severity:'error', summary:'Error', detail:'Please write correct Email'})
     }
-    this.as.sendMesage(data).subscribe((data)=>{
-      this.text="";
-      this.mail=""
-      this.msgs = [];
-      this.msgs.push({severity:'success', summary:'Success', detail:'Mail was sent succesfully'});
-         
-    })
+    else{
+      let data={
+        email:this.mail,
+        mess:this.text
+      }
+      this.as.sendMesage(data).subscribe((data)=>{
+        this.text="";
+        this.mail=""
+        this.msgs = [];
+        this.msgs.push({severity:'success', summary:'Success', detail:'Mail was sent succesfully'});                   
+      },
+      (err)=>{
+        this.msgs = [];
+        this.msgs.push({severity:'error', summary:'Error', detail:err.error}); 
+      })
+
+    }
+    
   }
 
 }
